6b131dbecebc93958b761edb115553e8b641776d
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2021-03-31 20:00:31 +0100

    feat: create comment on post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
     POST_ERROR,
     UPDATES_LIKES,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from '../actions/types'
 
 const initialState = {
@@ -63,6 +65,26 @@ const initialState = {
                 )),
                 loading: false
             };
+
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: payload
+                },
+                loading: false
+            };
+        
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id !== payload)
+                },
+                loading: false
+            }
 
         default:
             return state;
